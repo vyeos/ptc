767ed3e57ba51aeb42a912276fc9ff3fc6e1068f
vyeos <rudranpatel0980@gmail.com>
2026-05-22 20:14:33 +0530

    fix: only one instance

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -30,6 +30,13 @@ fn backup_database(app_handle: tauri::AppHandle, dest_folder: String) -> Result<
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
+            use tauri::Manager;
+            if let Some(window) = app.get_webview_window("main") {
+                let _ = window.unminimize();
+                let _ = window.set_focus();
+            }
+        }))
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_sql::Builder::default().build())
         .plugin(tauri_plugin_fs::init())
